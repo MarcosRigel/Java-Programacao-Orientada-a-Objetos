80f3a7c9f72ae8bf04ccbe26ac6468ef13da87e7
Marcos Rigel <marcosrigel99@gmail.com>
2023-03-16 00:04:43 -0300

    Funcoes (sintaxe)

---
+++ Logica de programacao/Secao 07 - Outros topicos basicos sobre java/demonstracao_04/src/course/Program.java
@@ -0,0 +1,54 @@
+/* Problema exemplo
+ * 
+ * Fazer um programa para ler três números inteiros e mostrar na tela o maior deles.
+ * 
+ * Exemplo:
+ * 
+ * Enter three numbers:
+ * 5
+ * 8
+ * 3
+ * Higher = 8
+ */
+
+package course;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int higher = max(a, b, c);
+		
+		showResult(higher);
+		
+		sc.close();
+	}
+
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		}
+		else if (y > z) {
+			aux = y;
+		}
+		else {
+			aux = z;
+		}
+		return aux;
+	}
+	
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
+	}
+
+}
