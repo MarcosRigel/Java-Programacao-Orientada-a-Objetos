0c36687ce6066b56cf86fc462001e09d16dfc95d
Marcos Rigel <marcosrigel99@gmail.com>
2023-03-25 14:12:15 -0300

    criando uma classe com tres atributos para representar melhor o triangulo

---
+++ POO - parte 01/Secao 08 - Introducao a programacao orientada a objetos/demonstracao_01/src/entities/Triangle.java
@@ -0,0 +1,12 @@
+// A classe é um tipo estruturado que pode conter (membros):
+// Atributos (dados / campos)
+// Métodos (funções / operações)
+
+package entities;
+
+public class Triangle {
+	
+	public double a;
+	public double b;
+	public double c;
+}
